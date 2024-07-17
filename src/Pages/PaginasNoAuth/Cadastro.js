758414efa1d3aba90df9d2015153f7bbf1b5c392
Ezequiel Sousa <63311919+Escowolf@users.noreply.github.com>
2024-07-17 06:26:13 -0300

    style: reorder elements

--- src/Pages/PaginasNoAuth/Cadastro.js
+++ src/Pages/PaginasNoAuth/Cadastro.js
@@ -1,29 +1,29 @@
 import { useState } from "react";
 import axios from "axios";
-import '../css/Cadastro.css'
+import './Cadastro.css'
 
 export function Cadastro() {
   const [email, setEmail] = useState("");
-  const [emailVerify,setEmailVerify] = useState("");
-  const [senha,setSenha] = useState("");
-  const [data,setDataNascimento] = useState("");
+  const [emailVerify, setEmailVerify] = useState("");
+  const [senha, setSenha] = useState("");
+  const [data, setDataNascimento] = useState("");
   const [nome, setNome] = useState("");
-  
 
-  function handleSubmit(e){
+
+  function handleSubmit(e) {
     e.preventDefault();
 
-      axios.post('http://localhost:4000/usuarios', {
-        email, senha, data, nome 
-      })
-      .then((res)=> (res.data))
+    axios.post('http://localhost:4000/usuarios', {
+      email, senha, data, nome
+    })
+      .then((res) => (res.data))
 
     alert("Usuário cadastrado com sucesso!");
 
     cleanAll();
   }
 
-  function cleanAll(){
+  function cleanAll() {
     setNome("");
     setEmail("");
     setEmailVerify("");
@@ -31,49 +31,51 @@ export function Cadastro() {
     setDataNascimento("");
   }
 
-  function comparaEmail(){
-//criar uma variável que exiba se os e-mails correspondem ou não;
-    if (email != emailVerify){
+  function comparaEmail() {
+    //criar uma variável que exiba se os e-mails correspondem ou não;
+    if (email !== emailVerify) {
       alert("Os e-mails não correspondem!");
-    }else{
+    } else {
       console.log("Tudo ok!")
     }
   }
 
   return (
     <div className="cadastro">
-      <div className="container">
-        
-        <br /><br />
-          <h1>Increva-se</h1>
-          <br /><br />
-          <form className="formBox" onSubmit={(e) => handleSubmit(e)}>
-            <input className="form-control form-control-lg" id="email" value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="E-mail" required/><br />
-            <input className="form-control form-control-lg" id="emailVerify"  value={emailVerify} type="email" onBlur={comparaEmail} onChange={(e) => setEmailVerify(e.target.value)} placeholder="Confirmar e-mail" required/><br />
-            <input className="form-control form-control-lg" id="password" type="password" value={senha} onChange={(e) => setSenha(e.target.value)} placeholder="Senha" required/><br />
-            <input className="form-control form-control-lg" id="nome" type="text" value={nome} onChange={(e) => setNome(e.target.value)} placeholder="Como devemos chamar você?" required/><br />
-            <label className="col-sm-10 col-form-label col-form-label-lg">Data de Nascimento</label><br />
-            <input className="form-control form-control-lg" id="dataNascimento" type="date" value={data}onChange={(e) => setDataNascimento(e.target.value)} placeholder="Data de Nascimento" required/><br />
+      <div className="section">
+        <h1>Inscreva-se</h1>
+        <form className="box-form" onSubmit={(e) => handleSubmit(e)}>
+          <input className="form-item" id="email" value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="E-mail" required /><br />
+          <input className="form-item" id="emailVerify" value={emailVerify} type="email" onBlur={comparaEmail} onChange={(e) => setEmailVerify(e.target.value)} placeholder="Confirmar e-mail" required /><br />
+          <input className="form-item" id="password" type="password" value={senha} onChange={(e) => setSenha(e.target.value)} placeholder="Senha" required /><br />
+          <input className="form-item" id="nome" type="text" value={nome} onChange={(e) => setNome(e.target.value)} placeholder="Como devemos chamar você?" required /><br />
+          <div className="form-choice">
+            <span>
+              <label className="form-item">Data de Nascimento</label>
+              <input className="form-item form-data" id="dataNascimento" type="date" value={data} onChange={(e) => setDataNascimento(e.target.value)} placeholder="Data de Nascimento" required />
+            </span>
+            <span>
+            <label className="form-item">Gênero</label>
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Feminino" />
+                <label className="form-check-label">Feminino</label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="Masculino<" />
+                <label className="form-check-label">Masculino</label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="Não-binário" />
+                <label className="form-check-label">Não-binárie</label>
+              </div>
+            </span>
+          </div>
 
-            <div className="form-check form-check-inline">
-              <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Feminino" />
-              <label className="form-check-label">Feminino</label>
-            </div>
-            <div className="form-check form-check-inline">
-              <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="Masculino<" />
-              <label className="form-check-label">Masculino</label>
-            </div>
-            <div className="form-check form-check-inline">
-              <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="Não-binário" />
-              <label className="form-check-label">Não-binário</label>
-            </div><br/><br/>
 
-
-          <center><button type="submit" value="Enviar" className="btn btn-primary btn-lg btn-block">Enviar</button></center>  
-          </form>
-        </div><br/><br/><br/>
+          <center><button type="submit" value="Enviar" className="btn btn-primary btn-lg btn-block">Enviar</button></center>
+        </form>
       </div>
-    
+    </div>
   );
 }
 
--- src/Pages/PaginasNoAuth/Home.js
+++ src/Pages/PaginasNoAuth/Home.js
@@ -4,13 +4,12 @@ import './Home.css';
 
 function Home() {
     return (
-        <div className="section">
-            <div className="intro">
-                <div className="column">
+        <div className="section grid-container">
+                <div className="grid-item area1">
                     <img src={happy} className="img-home" alt="happy people" />
                 </div>
 
-                <div className="column">
+                <div className="grid-item area2">
                     <h1><strong>Escute o mundo de um jeito diferente</strong></h1>
                     <h5 className='p_text'>Lorem ipsum dolor sit amet,
                         consectetur adipiscing elit. Maecenas ultricies
@@ -22,20 +21,16 @@ function Home() {
                         Sed et fermentum nibh, at pretium lorem.
                         Suspendisse id interdum arcu, semper fermentum metus.</h5>
                 </div>
+
+            <div className="banner grid-item area3">
+                <strong><p>Garanta já diversão para você, <br />amigos e familiares!</p></strong>
             </div>
-            <div className="bottom">
 
-                <div className="banner column">
-                    <strong><p>Garanta já diversão para você, <br />amigos e familiares!</p></strong>
-                </div>
-                <br />
-
-                <div className="banner_bottom column">
-                    <img src={concept} className="img_banner" alt="Disco music" />
-                    <div className="text_gif">
-                        <h5>Música de qualidade</h5>
-                        <p>Com o Umusic você consegue ouvir suas músicas sem se preocupar com ruídos!</p>
-                    </div>
+            <div className="banner_bottom grid-item area4">
+                <img src={concept} className="img_banner" alt="Disco music" />
+                <div className="text_gif">
+                    <h5>Música de qualidade</h5>
+                    <p>Com o Umusic você consegue ouvir suas músicas sem se preocupar com ruídos!</p>
                 </div>
             </div>
         </div>

294c198c921e3f251c7c995cbe6a7ebc5b74c893
Ezequiel Sousa <63311919+Escowolf@users.noreply.github.com>
2024-07-23 10:03:20 -0300

    style: add responsivite

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,20 +4,51 @@ import logo from "./favicon.optimized.svg"
 
 function Navbar() {
   return (<>
-    <nav className="nav-container">
+    <nav id="navbar" className="nav-container">
       <span className="logo">
         <img src={logo} className="logo_icon" alt="Logo site" />
         <Link to="/" className="logo_name">uMusic</Link>
       </span>
-      <i className="fa-solid fa-bars nav-icon"></i>
       <div className="nav-links">
-        <Link to="/" className="nav-item">Início</Link>
-        <Link to="/faq" className="nav-item">Faq</Link>
-        <Link to="/sigin" className="nav-item">Cadastro</Link>
-        <Link to="/login" className="nav-item">Login<i className="" /></Link>
+        <Link to="/" className="nav-item" onClick={hideItens}>Início</Link>
+        <Link to="/faq" className="nav-item" onClick={hideItens}>Faq</Link>
+        <Link to="/sigin" className="nav-item" onClick={hideItens}>Cadastro</Link>
+        <Link to="/login" className="nav-item" onClick={hideItens}>Login</Link>
       </div>
+      <span>
+        <Link to="#" className="nav-icon nav-item" onClick={lightModeFunction}>
+          <i id="light-mode" className="fa-regular fa-lightbulb" alt="Toggle light mode" value="1"></i>
+        </Link>
+
+        <Link to="#" className="dropbtn nav-icon nav-item" onClick={showItens}>
+          <i className="fa fa-bars"></i>
+        </Link>
+      </span>
     </nav>
   </>);
+
+  //Função que altera tema da página. 
+  //A variável element serve para definir qual elemento será alterado.
+  function lightModeFunction() {
+    var element = document.documentElement;
+    element.classList.toggle("light-mode");
+  }
+
+  function showItens() {
+    var x = document.getElementById("navbar");
+    if (x.className === "nav-container") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-container";
+    }
+  }
+
+  function hideItens() {
+    var x = document.getElementById("navbar");
+    if (x.className.includes("responsive")) {
+      x.className = "nav-container";
+    }
+  }
 }
 
 export default Navbar;
